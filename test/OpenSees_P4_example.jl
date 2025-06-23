fae96df9d9a2d99a91bb01ef5813325911594a08
Cris Moen <cris.moen@runtosolve.com>
2025-06-23 19:35:49 +0300

    add example from OpenSees website

--- test/OpenSees_P4_example.jl
+++ test/OpenSees_P4_example.jl
@@ -1,21 +1,36 @@
 using Pinching4Model, CairoMakie 
 
 
-half_cycles = [[0.0, 0.0001], 
-           [-0.0001, 0.001], 
-           [-0.001, 0.002], 
-           [-0.002, 0.003], 
-           [-0.003, 0.005], 
-           [-0.005, 0.006], 
-           [-0.006, 0.007], 
-           [-0.007, 0.009],
-            [-0.009, 0.01],
-             [-0.01, 0.011], 
-             [-0.011, 0.012], 
-             [-0.012, 0.013], 
-             [-0.013, 0.0]]
+#Consider this OpenSees example:
+#https://opensees.berkeley.edu/wiki/index.php/Pinching4MaterialExample
 
-incr = 10
+set peakpts [list 0.0001 0.001 0.002 0.003 0.005 0.006 0.007 0.009 0.01 0.011 0.012 0.013 ]
+
+
+half_cycles = [[0.0, 0.0001], 
+           [0.0001, -0.0001], 
+           [-0.0001, 0.001],
+           [0.001, -0.001], 
+            [-0.001, 0.002],
+            [0.002, -0.003],
+            [-0.003, 0.003],
+            [0.003, -0.005],
+            [-0.005, 0.006],
+             [0.006, -0.006],
+              [-0.006, 0.007],
+              [0.007, -0.007],
+              [-0.007, 0.009],
+              [0.009, -0.009],
+              [-0.009, 0.010],
+              [0.010, -0.010],
+               [-0.010, 0.011],
+               [0.011, -0.011],
+               [-0.011, 0.012],
+               [0.012, -0.012],
+               [-0.012, 0.013],
+               [0.013, 0.0]]
+
+incr = 300
 
 d = []
 
@@ -31,7 +46,11 @@ for i in eachindex(half_cycles)
 
 end
 
+scatterlines(1:length(d), d)
 
+
+
+### no damage plot
 dmgtype = "energy"
 
 strain1p = 0.0001
@@ -66,17 +85,36 @@ gammaK3 = 0.3
 gammaK4 = 0.2
 gammaKLimit = 0.9
 
+gammaK1 = 0.0
+gammaK2 = 0.0
+gammaK3 = 0.0
+gammaK4 = 0.0
+gammaKLimit = 0.0
+
+
 gammaD1 = 0.5
 gammaD2 = 0.5
 gammaD3 = 2.0
 gammaD4 = 2.0
 gammaDLimit = 0.5
 
+gammaD1 = 0.0
+gammaD2 = 0.0
+gammaD3 = 0.0
+gammaD4 = 0.0
+gammaDLimit = 0.0
+
 gammaF1 = 1.0
 gammaF2 = 0.0
 gammaF3 = 1.0
 gammaF4 = 1.0
 gammaFLimit = 0.9
+
+gammaF1 = 0.0
+gammaF2 = 0.0
+gammaF3 = 0.0
+gammaF4 = 0.0
+gammaFLimit = 0.0
 
 
 gE = 10
@@ -141,70 +179,154 @@ inputs = Pinching4Model.Inputs(
 force = Pinching4Model.response(inputs)
 
 
-scatterlines(d, force)
+f = Figure()
+ax = Axis(f[1, 1])
+
+
+lines!(ax, d, force, color=:red)
+ylims!(-8, 8)
+xlims!(-0.015, 0.015)
+f
+
+
+###
 
 
 
-# ##### Positive/Negative envelope Stress/Load
 
-# ### stress1 stress2 stress3 stress4
+### damage plot
+dmgtype = "energy"
 
-# pEnvelopeStress = [2.0, 6.0, 7.0, 0.2]
+strain1p = 0.0001
+strain2p = 0.0055
+strain3p = 0.0188
+strain4p = 0.0189
+strain1n = -0.0001
+strain2n = -0.0055
+strain3n = -0.0188
+strain4n = -0.0189
 
-# nEnvelopeStress = [-2.0, -6.0, -7.0, -0.2]
+stress1p = 2.0
+stress2p = 6.0
+stress3p = 7.0
+stress4p = 0.2
+stress1n = -2.0
+stress2n = -6.0
+stress3n = -7.0
+stress4n = -0.2
 
-# ##### Positive/Negative envelope Strain/Deformation
+rDispP = 0.5
+rForceP = 0.25
+uForceP = 0.05
+rDispN = 0.5
+rForceN = 0.25
+uForceN = 0.05
 
-# ### strain1 strain2 strain3 strain4
 
-# pEnvelopeStrain = [0.0001, 0.0055, 0.0188, 0.0189]
+gammaK1 = 1.0
+gammaK2 = 0.2
+gammaK3 = 0.3
+gammaK4 = 0.2
+gammaKLimit = 0.9
 
-# nEnvelopeStrain = [-0.0001, -0.0055, -0.0188, -0.0189]
+# gammaK1 = 0.0
+# gammaK2 = 0.0
+# gammaK3 = 0.0
+# gammaK4 = 0.0
+# gammaKLimit = 0.0
 
-# ##### Ratio of maximum deformation at which reloading begins
 
-# ### Pos_env. Neg_env.
+gammaD1 = 0.5
+gammaD2 = 0.5
+gammaD3 = 2.0
+gammaD4 = 2.0
+gammaDLimit = 0.5
 
-# rDisp = [0.5, 0.5]
+# gammaD1 = 0.0
+# gammaD2 = 0.0
+# gammaD3 = 0.0
+# gammaD4 = 0.0
+# gammaDLimit = 0.0
 
-# ##### Ratio of envelope force (corresponding to maximum deformation) at which reloading begins
+gammaF1 = 1.0
+gammaF2 = 0.0
+gammaF3 = 1.0
+gammaF4 = 1.0
+gammaFLimit = 0.9
 
-# ### Pos_env. Neg_env.
+# gammaF1 = 0.0
+# gammaF2 = 0.0
+# gammaF3 = 0.0
+# gammaF4 = 0.0
+# gammaFLimit = 0.0
 
-# rForce = [0.25, 0.25]
 
-# ##### Ratio of monotonic strength developed upon unloading
+gE = 10
 
-# ### Pos_env. Neg_env.
 
-# uForce = [0.05, 0.05]
 
-# ##### Coefficients for Unloading Stiffness degradation
+inputs = Pinching4Model.Inputs(
+    d,
+    dmgtype,
 
-# ### gammaK1 gammaK2 gammaK3 gammaK4 gammaKLimit
 
-# gammaK = [1.0, 0.2, 0.3, 0.2, 0.9]
+    strain1p,
+    strain2p,
+    strain3p,
+    strain4p,
+    strain1n,
+    strain2n,
+    strain3n,
+    strain4n,
 
-# # gammaK [0.0, 0.0, 0.0, 0.0, 0.0]
+    stress1p,
+    stress2p,
+    stress3p,
+    stress4p,
+    stress1n,
+    stress2n,
+    stress3n,
+    stress4n,
 
-# ##### Coefficients for Reloading Stiffness degradation
 
-# ### gammaD1 gammaD2 gammaD3 gammaD4 gammaDLimit
+    rDispP,
+    rForceP,
+    uForceP,
+    rDispN,
+    rForceN,
+    uForceN,
 
-# gammaD =[0.5, 0.5, 2.0, 2.0, 0.5]
 
-# #set gammaD [list 0.0 0.0 0.0 0.0 0.0]
+    gammaK1,
+    gammaK2,
+    gammaK3,
+    gammaK4,
+    gammaKLimit,
 
-# ##### Coefficients for Strength degradation
+    gammaD1,
+    gammaD2,
+    gammaD3,
+    gammaD4,
+    gammaDLimit,
 
-# ### gammaF1 gammaF2 gammaF3 gammaF4 gammaFLimit
+    gammaF1,
+    gammaF2,
+    gammaF3,
+    gammaF4,
+    gammaFLimit,
 
-# gammaF = [1.0, 0.0, 1.0, 1.0, 0.9]
+    gE
+)
 
-# #set gammaF [list 0.0 0.0 0.0 0.0 0.0]
 
-# gammaE = 10
 
-# # damage type (option: "energy", "cycle")
+force_damage = Pinching4Model.response(inputs)
 
-# dam = "energy"
+
+
+
+
+lines!(ax, d, force_damage, color=:blue)
+ylims!(-8, 8)
+xlims!(-0.015, 0.015)
+f
